20210422de21c9d70a456b44da20795009cae0b6
Kristian Mandrup <kmandrup@gmail.com>
2010-04-21 06:55:41 -0400

    added migration option to generators

--- lib/generators/auth_assistant/config_generator.rb
+++ lib/generators/auth_assistant/config_generator.rb
@@ -5,6 +5,10 @@ module AuthAssistant
     class ConfigGenerator < Rails::Generators::NamedBase
       desc "Generates user role migration and matching auth configuration initializer" 
 
+      class_option :migration, :type => :boolean, :aliases => "-m", :default => false,
+                                     :desc => "To generate a user role migration."
+
+
       def self.source_root
         @_devise_source_root ||= File.expand_path("../templates", __FILE__)
       end
@@ -14,6 +18,7 @@ module AuthAssistant
       end 
 
       def run_migration
+        return nil if !options[:migration]        
         send :"#{name}_migration"          
       end 
       
