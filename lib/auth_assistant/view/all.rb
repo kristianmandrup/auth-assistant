9834f7a04c62ad819dff9a6a4b1e36bb6c0b943c
Kristian Mandrup <kmandrup@gmail.com>
2010-05-16 11:19:55 -0400

    fixed some bugs

--- lib/auth_assistant/view/all.rb
+++ lib/auth_assistant/view/all.rb
@@ -1,3 +1,4 @@
-require 'auth_assistant/view/auth_link'
 require 'auth_assistant/view/auth_menu_item'
 require 'auth_assistant/view/rest_link'
+require 'auth_assistant/view/registration_link'
+require 'auth_assistant/view/session_link'
