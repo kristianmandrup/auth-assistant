d734ccce9709e23535ff6d7e7015739d4c1c7083
Kristian Mandrup <kmandrup@gmail.com>
2010-08-26 17:59:03 +0200

    all rest links work

--- spec/auth-assistant/link/rest_spec.rb
+++ spec/auth-assistant/link/rest_spec.rb
@@ -25,78 +25,91 @@ describe AuthAssistant::Link::Rest do
 
         res = e.run_template_locals :post => post, :name => label do 
           %{<%= index_link(post, name) %> }
-        end
+        end 
+        res.should match /it works/
       end                     
     end
   end
   
-  # describe '#create_link' do
-  #   it "should create a create link" do
-  #     with_action_view do |view|           
-  #       label = 'create'
-  #       path = 'post/create'        
-  #       post = Post.new
-  # 
-  #       view.stubs(:auth_labels).returns(:index => label)
-  # 
-  #       view.stubs(:new_post_path).returns path
-  #       view.stubs(:can?).returns true
-  #       view.stubs(:link_to).with(label, path).returns 'it works'        
-  # 
-  #       view.create_link(post, label).should == 'it works'
-  #     end                     
-  #   end
-  # end
-  # 
-  # describe '#edit_link' do
-  #   it "should create a create link" do
-  #     with_action_view do |view|           
-  #       label = 'create'
-  #       path = 'post/create'        
-  #       post = Post.new
-  # 
-  #       view.stubs(:auth_labels).returns(:index => label)
-  # 
-  #       view.stubs(:can?).returns true
-  #       view.stubs(:link_to).with(label, [:edit, post]).returns 'it works'        
-  # 
-  #       view.edit_link(post, label).should == 'it works'
-  #     end                     
-  #   end
-  # end
-  # 
-  # describe '#delete_link' do
-  #   it "should create a delete link" do
-  #     with_action_view do |view|           
-  #       label = 'delete'
-  #       path = 'post/delete'        
-  #       post = Post.new
-  # 
-  #       view.stubs(:auth_labels).returns(:index => label)
-  # 
-  #       view.stubs(:can?).returns true
-  #       view.stubs(:link_to).returns 'it works'        
-  # 
-  #       view.delete_link(post, label).should == 'it works'
-  #     end                     
-  #   end
-  # end
-  # 
-  # 
-  # describe '#show_link' do
-  #   it "should create a show link" do
-  #     with_action_view do |view|           
-  #       label = 'show'
-  #       path = 'post/show'        
-  #       post = Post.new
-  # 
-  #       view.stubs(:auth_labels).returns(:index => label)
-  #       view.stubs(:can?).returns true
-  #       view.stubs(:link_to).with(label, post).returns 'it works'        
-  # 
-  #       view.show_link(post, label).should == 'it works'
-  #     end                     
-  #   end
-  # end
+  describe '#create_link' do
+    it "should create a create link" do
+      with_engine do |e, view|
+        label = 'create'
+        path = 'post/create'        
+        post = Post.new
+  
+        view.stubs(:auth_labels).returns(:index => label)
+  
+        view.stubs(:new_post_path).returns path
+        view.stubs(:can?).returns true
+        view.stubs(:link_to).with(label, path).returns 'it works'        
+  
+        res = e.run_template_locals :post => post, :name => label do 
+          %{<%= create_link(post, name) %> }
+        end
+        res.should match /it works/
+      end                     
+    end
+  end
+  
+  describe '#edit_link' do
+    it "should create a create link" do
+      with_engine do |e, view|
+        label = 'create'
+        path = 'post/create'        
+        post = Post.new
+  
+        view.stubs(:auth_labels).returns(:index => label)
+  
+        view.stubs(:can?).returns true
+        view.stubs(:link_to).with(label, [:edit, post]).returns 'it works'        
+  
+        res = e.run_template_locals :post => post, :name => label do 
+          %{<%= edit_link(post, name) %> }
+        end
+        res.should match /it works/
+      end                     
+    end
+  end
+  
+  describe '#delete_link' do
+    it "should create a delete link" do
+      with_engine do |e, view|
+        label = 'delete'
+        path = 'post/delete'        
+        post = Post.new
+  
+        view.stubs(:auth_labels).returns(:index => label)
+  
+        view.stubs(:can?).returns true
+        view.stubs(:link_to).returns 'it works'        
+  
+        res = e.run_template_locals :post => post, :name => label do 
+          %{<%= delete_link(post, name) %> }
+        end
+        res.should match /it works/
+      end                     
+    end
+  end
+  
+  
+  describe '#show_link' do
+    it "should create a show link" do
+      with_engine do |e, view|
+        label = 'show'
+        path = 'post/show'        
+        post = Post.new
+  
+        view.stubs(:auth_labels).returns(:index => label)
+        view.stubs(:can?).returns true
+        view.stubs(:link_to).with(label, post).returns 'it works'        
+
+        res = e.run_template_locals :post => post, :name => label do 
+          %{<%= show_link(post, name) %> }
+        end
+        res.should match /it works/
+      end                     
+    end
+  end
 end
 
