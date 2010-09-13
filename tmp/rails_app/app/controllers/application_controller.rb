0c2ef9d5da028ae78ee1ad825850a31ddb7895a3
Kristian Mandrup <kmandrup@gmail.com>
2010-09-13 23:03:56 +0200

    deleted temp dir

--- tmp/rails_app/app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  
-rescue_from CanCan::AccessDenied do |exception|
-  flash[:error] = exception.message
-  redirect_to root_url
-end
-
-  protect_from_forgery
-end
--- tmp/rails_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsApp::Application.config.secret_token = '6d017690a45671c8cac2fb9d649247518d54aad80372c8fea8cf6069d125459fa1810abc50b3591d1b6a2ac546eaa076d327d5f58b834b2351cd96070282023c'
